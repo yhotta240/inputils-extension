14538749574c76af2b21bc9132e52c1e9098f4eb
yhotta240 <yhotta240@icloud.com>
2025-12-14 15:19:11 +0900

    feat(commands): コマンド取得とクエリ処理を追加

---
+++ src/content/commands.ts
@@ -0,0 +1,29 @@
+import { CONFIG, FeatureKey } from "../settings";
+
+const features = CONFIG.features;
+
+// 指定されたコマンド文字を取得
+export function getCommand(featureKey: FeatureKey): string {
+  return features[featureKey].char;
+};
+
+// コマンドに続くクエリ部分を取得
+export function getCommandQuery(text: string, featureKey: FeatureKey): string | null {
+  const command = getCommand(featureKey);
+  const position = features[featureKey].position as ('start' | 'end')[];
+
+  // start が許可されていて，先頭にある場合
+  if (position.includes('start') && text.startsWith(command)) {
+    return text.substring(command.length);
+  }
+
+  // end が許可されていて，text 内に存在する場合
+  if (position.includes('end')) {
+    const lastIndex = text.lastIndexOf(command);
+    if (lastIndex !== -1) {
+      return text.substring(lastIndex + command.length);
+    }
+  }
+
+  return null;
+};
